9c8776bd8c8f0dee405ebd64b78d033c354e7d9e
NobuhiroKato <n-kato@nekojarashi.com>
2020-03-21 18:32:46 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   # 保存前に email を小文字に変換
-  before_save { self.email = email.downcase }
+  before_save downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -20,6 +21,16 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_digest)
   end
 
   def forgot
